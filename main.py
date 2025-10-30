255b4b36a8a25faee4a9bb120503260088865250
minmin2104 <aminkafri0045@gmail.com>
2025-10-30 12:34:07 +0800

    prepare pygame screen

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import pygame
+
+
 class Game:
-    def __init__(self):
-        print("Hello, World!")
+    def __init__(self, win_width, win_height, win_title):
+        self.snake = None
+        self.food = None
+        self.score = 0
 
+        self.fps = 60
+        self.game_state = None
+        self.running = True
 
+        pygame.init()
+        self.__win_width = win_width
+        self.__win_height = win_height
+        self.screen = pygame.display.set_mode((win_width, win_height))
+        pygame.display.set_caption(win_title)
+        self.clock = pygame.time.Clock()
+
+    def main(self):
+        while self.running:
+            # Poll input
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # Update & Render
+            self.screen.fill((0, 0, 0))
+            
+            pygame.display.flip()
+
+            self.clock.tick(self.fps)
+
+    def clean_up(self):
+        pygame.quit()
+    
+        
 if __name__ == "__main__":
-    Game()
+    game = Game(800, 600, "Da Snake")
+    game.main()
+    game.clean_up()
